c7581ca669463ed815912e6a154025221353f518
Harshvardhan1909 <115179635+Harshvardhan1909@users.noreply.github.com>
2023-01-29 12:02:16 +0530

    add

--- src/main/java/com/driver/Application.java
+++ src/main/java/com/driver/Application.java
@@ -1,12 +1,26 @@
 package com.driver;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class Group {
+	private String name;
+	private int numberOfParticipants;
+	public Group(String name, int numberOfParticipants) {
+		this.name = name;
+		this.numberOfParticipants = numberOfParticipants;
+	}
 
-@SpringBootApplication
-public class Application {
+	public String getName() {
+		return name;
+	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumberOfParticipants() {
+		return numberOfParticipants;
+	}
+
+	public void setNumberOfParticipants(int numberOfParticipants) {
+		this.numberOfParticipants = numberOfParticipants;
 	}
 }
